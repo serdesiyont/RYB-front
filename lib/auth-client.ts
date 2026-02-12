7aa061eea87d4de8ccf69524d1455e96989a852b
serdesiyonts <serdesiyon@gmail.com>
2026-02-12 16:31:49 +0300

    better-auth issue

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,8 @@
-import {createAuthClient} from 'better-auth/react';
+import { createAuthClient } from "better-auth/react";
+
+
+const apiBase = process.env.NEXT_PUBLIC_APP_URL;
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000/api/auth',
+  baseURL: `${apiBase}/api/auth`,
 });
